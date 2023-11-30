4fe21a4a81bf836f2332542f15e9a5748614a47e
prasenjitsahamfg <p.saha@mfinancialg.com>
2023-12-01 00:40:25 +0530

    git work push

--- src/app/api/getStockPrice/route.js
+++ src/app/api/getStockPrice/route.js
@@ -1,31 +1,33 @@
 export async function GET(req) {
-  const searchParams = await req?.nextUrl?.searchParams;
+  const searchParams = req?.nextUrl?.searchParams;
   const name = searchParams?.get("stock_name");
-  const max = 126;
-  const min = 124;
-  const stock_price = (Math.random(0, 1) * (max - min) + min).toFixed(2);
-  const data = [
-    {
-      stock: "idfc",
-      price: parseFloat(stock_price),
-    },
-    {
-      stock: "sbi",
-      price: 424.26,
-    },
-    {
-      stock: "yesbank",
-      price: 200.75,
-    },
-  ];
-
+  const data = await database();
   if (!name) {
     return Response.json(data);
   }
-
   const result = data.filter(
     (e) => e.stock.toLowerCase() == name.toLowerCase()
   );
-
   return Response.json(result?.[0]);
+}
+
+function stockPrice(max, min) {
+  return (Math.random(0, 1) * (max - min) + min).toFixed(2);
+}
+
+function database() {
+  return [
+    {
+      stock: "idfc",
+      price: parseFloat(stockPrice(126, 124)),
+    },
+    {
+      stock: "sbi",
+      price: parseFloat(stockPrice(424, 436)),
+    },
+    {
+      stock: "yesbank",
+      price: parseFloat(stockPrice(200, 210)),
+    },
+  ];
 }
