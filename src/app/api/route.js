d83c0caa5c307f71ff40b6dce2b75afc31271a04
prasenjitsahamfg <p.saha@mfinancialg.com>
2023-12-03 01:17:13 +0530

    git work

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,8 +1,29 @@
-function data() {
-  return "Prasenjit Saha";
+// pages/api/your-api-route.js
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
 }
 
-export async function GET(req) {
-  //   console.log(request);
-  return Response.json({ data: "Prasenjit Saha" });
+// Your API route
+export default async function handler(req, res) {
+  // Run cors
+  await runMiddleware(req, res, cors);
+
+  // Your API logic here
+  res.status(200).json({ message: "Hello, World!" });
 }
